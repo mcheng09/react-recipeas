7291984ae471589b99465da78c69213f1c2fb940
Michael <mcheng.oc@gmail.com>
2020-04-28 12:01:25 -0700

    create(Layout): creates layout and aux component

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Aux from './../Aux/Aux'
+
+function Layout(props) {
+  return (
+    <Aux>
+      <h1>Happy Hacking!</h1>
+    </Aux>
+  )
+}
+
+export default Layout;
