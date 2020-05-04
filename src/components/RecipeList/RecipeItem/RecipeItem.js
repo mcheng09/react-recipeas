eebbbba2e1eb309ec85b47d97ab9e90360130229
Michael <mcheng.oc@gmail.com>
2020-05-04 13:54:44 -0700

    create(RecipeItem): creates component and stylesheet

--- src/components/RecipeList/RecipeItem/RecipeItem.js
+++ src/components/RecipeList/RecipeItem/RecipeItem.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import RecipeIngredients from './RecipeIngredients/RecipeIngredients'
+import styles from './RecipeItem.module.css'
+
 const RecipeItem = (props) => {
   return (
-    <div>{props.recipe.recipe.label}</div>
+    <div className={styles.RecipeItem}>
+      <img src={props.recipe.recipe.image} alt={props.recipe.recipe.label}/>
+      <div>
+        <h3>{props.recipe.recipe.label}</h3>
+        <p>Calories: {props.recipe.recipe.calories.toFixed(0)}</p>
+        <RecipeIngredients ingredients={props.recipe.recipe.ingredientLines}/>
+      </div>
+    </div>
   )
 }
 
