c9b1b65d9582975083664466fa2c95da29fc90d9
Michael <mcheng.oc@gmail.com>
2020-05-04 13:53:23 -0700

    create(api): creates api and sets link for api call

---
+++ src/store/api.js
@@ -0,0 +1,8 @@
+const EDAMAM_APP_ID = process.env.REACT_APP_EDAMAM_APP_ID
+const EDAMAM_API_KEY = process.env.REACT_APP_EDAMAM_API_KEY
+
+const api = () => {
+  return `https://api.edamam.com/search?app_id=${EDAMAM_APP_ID}&app_key=${EDAMAM_API_KEY}&from=0&to=25&q=`
+}
+
+export default api();
