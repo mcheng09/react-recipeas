3d38e3f6771fa4591d92064ea4abf61265e5751a
Michael <mcheng.oc@gmail.com>
2020-05-04 13:44:05 -0700

    create(Navigation): creates component and stylesheet

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './Navigation.module.css'
+
+const Navigation = () => {
+  return (
+    <div className={styles.Navigation}>
+      <h1>RecipEaze</h1>
+    </div>
+  )
+}
+
+export default Navigation;
