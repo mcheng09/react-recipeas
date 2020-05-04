befde1c8a05e43338f72b24cbd61f547f3c7a0bb
Michael <mcheng.oc@gmail.com>
2020-05-04 13:59:10 -0700

    update(RecipeSearch): adds styling classes

--- src/components/RecipeSearch/RecipeSearch.js
+++ src/components/RecipeSearch/RecipeSearch.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 
+import styles from './RecipeSearch.module.css'
+
 const RecipeSearch = (props) => {
   const [searchRecipe, setSearchRecipe] = useState('')
 
   return (
-    <div>
+    <div className={styles.RecipeSearch}>
       <form>
-        <input placeholder='Search Recipe' style={{ marginRight: '8px'}} onChange={(e) => setSearchRecipe(e.target.value)}/>
+        <input className={styles.RecipeSearchInput} placeholder='Search Recipe' onChange={(e) => setSearchRecipe(e.target.value)}/>
         <input type='submit' value='Search!' onClick={e => props.clickHelper(e, searchRecipe)} />
+        <input type='reset' value='Reset Search'/>
       </form>
     </div>
   )
