0970e049477b5da059dcf046257e0e29d23b22ff
Michael <mcheng.oc@gmail.com>
2020-05-04 13:54:10 -0700

    update(Layout): adds Navigation to Layout

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Aux from './../Aux/Aux'
+import Navigation from './../../components/Navigation/Navigation'
 import RecipeContainer from './../../containers/RecipeContainer/RecipeContainer'
 
 function Layout(props) {
   return (
     <Aux>
+      <Navigation />
       <RecipeContainer />
     </Aux>
   )
