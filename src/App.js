03b0aa53b713e89ff58773b232da5ba09e1a7d75
Michael <mcheng.oc@gmail.com>
2020-05-06 16:24:48 -0700

    feat(App): adds router views to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import Layout from './hoc/Layout/Layout'
+import HomePage from './pages/Home/Home';
+import AboutPage from './pages/About/About'
+import RecipeContainer from './containers/RecipeContainer/RecipeContainer'
 
 function App() {
   return (
     <div className="App">
-      <Layout />
+      <BrowserRouter>
+        <Layout>
+            <Switch>
+              <Route path='/recipes' component={RecipeContainer} />
+              <Route path='/about' component={AboutPage} />
+              <Route path='/' component={HomePage} />
+            </Switch>
+        </Layout>
+      </BrowserRouter>
     </div>
   );
 }
