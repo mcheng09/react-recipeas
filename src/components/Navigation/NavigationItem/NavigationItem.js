08c8b686d0fab43d7eebb51885df8db5e1bd25b5
Michael <mcheng.oc@gmail.com>
2020-05-12 12:30:34 -0700

    feat(NavigationItem): creates links to all pages and container

--- src/components/Navigation/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItem/NavigationItem.js
@@ -6,9 +6,10 @@ import styles from './NavigationItem.module.scss';
 const NavigationItem = () => {
   return (
     <div>
-      <Link className={styles.NavLink} to='/'>Home</Link>
-      <Link className={styles.NavLink} to='/about'>About</Link>
-      <Link className={styles.NavLink} to='/recipes'>Search Recipe</Link>
+      <Link className={styles.NavLink} to='/'><span className={styles.NavIcon}><i class="fas fa-home"></i></span><span className={styles.NavText}>Home</span></Link>
+      <Link className={styles.NavLink} to='/about'><span className={styles.NavIcon}><i class="fas fa-cookie-bite"></i></span><span className={styles.NavText}>About</span></Link>
+      <Link className={styles.NavLink} to='/recipes'><span className={styles.NavIcon}><i class="fas fa-book"></i></span><span className={styles.NavText}>Search Recipes</span></Link>
+      <Link className={styles.NavLink} to='/contact'><span className={styles.NavIcon}><i class="fas fa-address-book"></i></span><span className={styles.NavText}>Contact</span></Link>
     </div>
   )
 }
