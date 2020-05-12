7eb5b581077812ccc8e331d3ff50769483e835d5
Michael <mcheng.oc@gmail.com>
2020-05-12 12:30:51 -0700

    updates(App): adds styling and color to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 
 import Layout from './hoc/Layout/Layout'
 import HomePage from './pages/Home/Home';
