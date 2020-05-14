1a9e501d98b465965f2886fb5f7f0bd5575f6e23
Michael <mcheng.oc@gmail.com>
2020-05-13 17:51:16 -0700

    update(RecipeSearch): adds margin to containing elements

--- src/components/RecipeSearch/RecipeSearch.js
+++ src/components/RecipeSearch/RecipeSearch.js
@@ -2,13 +2,14 @@ import React, { useState } from 'react';
 
 import Form from 'react-bootstrap/form'
 import Button from 'react-bootstrap/button'
+import styles from './RecipeSearch.module.scss'
 
 const RecipeSearch = (props) => {
   const [searchRecipe, setSearchRecipe] = useState('')
 
   return (
-    <Form>
-      <Form.Control type='text' placeholder='Search Recipe' onChange={(e) => setSearchRecipe(e.target.value)}/>
+    <Form className={styles.SearchForm}>
+      <Form.Control className={styles.SearchInput} type='text' placeholder='Search Recipe' onChange={(e) => setSearchRecipe(e.target.value)}/>
       <Button type='submit' onClick={e => props.clickHelper(e, searchRecipe)}>Search</Button>
       <Button type='reset'>Reset</Button>
     </Form>
