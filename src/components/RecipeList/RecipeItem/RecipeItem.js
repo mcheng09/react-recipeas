ee15a563938420f0201b3f3d74f95208f3606077
Michael <mcheng.oc@gmail.com>
2020-05-13 18:00:05 -0700

    update(RecipeItem): adds styling to component

--- src/components/RecipeList/RecipeItem/RecipeItem.js
+++ src/components/RecipeList/RecipeItem/RecipeItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import RecipeIngredients from './RecipeIngredients/RecipeIngredients'
-import styles from './RecipeItem.module.css'
+import styles from './RecipeItem.module.scss'
 
 const RecipeItem = (props) => {
   return (
     <div className={styles.RecipeItem}>
-      <img src={props.recipe.recipe.image} alt={props.recipe.recipe.label}/>
+      <div className={styles.RecipeImage} style={{ backgroundImage: `url(${props.recipe.recipe.image})`}} alt={props.recipe.recipe.label} ></div>
       <div>
         <h3>{props.recipe.recipe.label}</h3>
         <p>Calories: {props.recipe.recipe.calories.toFixed(0)}</p>
