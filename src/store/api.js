e0bfa17f3288f97dd7e6564010fb71bf0ea42531
Michael <mcheng.oc@gmail.com>
2020-05-13 18:04:15 -0700

    update(api): updates api to call 10 items

--- src/store/api.js
+++ src/store/api.js
@@ -2,7 +2,7 @@ const EDAMAM_APP_ID = process.env.REACT_APP_EDAMAM_APP_ID
 const EDAMAM_API_KEY = process.env.REACT_APP_EDAMAM_API_KEY
 
 const api = () => {
-  return `https://api.edamam.com/search?app_id=${EDAMAM_APP_ID}&app_key=${EDAMAM_API_KEY}&from=0&to=25&q=`
+  return `https://api.edamam.com/search?app_id=${EDAMAM_APP_ID}&app_key=${EDAMAM_API_KEY}&from=0&to=10&q=`
 }
 
 export default api();
